f2ab647b4c7a0a29433881d6b7c0d8013c000458
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-04-04 08:24:34 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './index.css'; /* 디자인을 불러오는 이 줄을 추가합니다 */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
